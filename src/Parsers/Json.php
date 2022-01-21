335fc22a7192afa1e117eb8bcae11d2056764c40
funkylen <funkylen@gmail.com>
2022-01-21 12:12:20 +0300

    add file read fail exception

--- src/Parsers/Json.php
+++ src/Parsers/Json.php
@@ -5,5 +5,10 @@ namespace Differ\Parsers\Json;
 function getFileContents(string $path): array
 {
     $content = file_get_contents($path);
+
+    if (!$content) {
+        throw new \Exception("Can't read file contents :(");
+    }
+
     return json_decode($content, true);
 }
