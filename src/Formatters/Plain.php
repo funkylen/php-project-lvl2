e2c899e270643a3db5eefd5dee1fa63940a44f78
funkylen <funkylen@gmail.com>
2022-01-21 12:30:34 +0300

    fix

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -8,9 +8,7 @@ use function Differ\DiffBuilder\getOldValue;
 use function Differ\DiffBuilder\getValue;
 use function Differ\DiffBuilder\isAddedNode;
 use function Differ\DiffBuilder\isDiff;
-
 use function Differ\DiffBuilder\isRemovedNode;
-
 use function Differ\DiffBuilder\isUntouchedNode;
 use function Differ\DiffBuilder\isUpdatedNode;
 
