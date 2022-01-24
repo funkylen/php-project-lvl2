1a3474c85adeb457d4011d723a59aeb3cf015509
funkylen <funkylen@gmail.com>
2022-01-24 23:14:38 +0300

    fix

--- src/Parsers.php
+++ src/Parsers.php
@@ -4,19 +4,6 @@ namespace Differ\Parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function parse(string $path, $extension): array
-{
-    if ($extension === 'json') {
-        return parseJson($path);
-    }
-
-    if (in_array($extension, ['yml', 'yaml'], true)) {
-        return parseYaml($path);
-    }
-
-    throw new \Exception('undefined format');
-}
-
 function parseJson(string $content): array
 {
     return json_decode($content, true);
